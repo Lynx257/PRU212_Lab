1cd33a71669ab9c1e478e09a6caf11148f25db79
Lynx257 <uchihachirido257@gmail.com>
2025-03-02 17:48:42 +0700

    Add canvas for project
    Canvas include player heart and timer

---
+++ PRU212_Lab/Assets/Scripts/Timer.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public float timeRemaining = 0;
+    public bool timeIsRunning = true;
+    public TMP_Text timeText;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        timeIsRunning = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeIsRunning)
+        {
+            if(timeRemaining >= 0)
+            {
+                timeRemaining += Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+        }
+    }
+
+    void DisplayTime (float timeToDisplay)
+    {
+        timeToDisplay += 1;
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        timeText.text = string.Format("{00:00}:{01:00}", minutes, seconds);
+    }
+}
